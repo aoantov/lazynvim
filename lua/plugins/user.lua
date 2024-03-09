2e6e84d18eb70047de95c78a57f2d853adc9876f
Anton Antov <anton.antov@softwareone.com>
2024-03-09 10:48:18 +0200

    update lazynvim config

---
+++ lua/plugins/user.lua
@@ -0,0 +1,15 @@
+--  Basic Plugins
+return {
+	{
+		"folke/zen-mode.nvim",
+		cmd = "ZenMode",
+		keys = { { "<leader>z", "<cmd>ZenMode<cr>", desc = "Zen Mode" } },
+	},
+	{
+		"norcalli/nvim-colorizer.lua",
+		event = "BufEnter",
+		keys = { { "<C-c>", "<cmd>ColorizerToggle<cr>", desc = "CSS Coloring" } },
+		ft = { "css" },
+	},
+	{ "tpope/vim-dadbod" },
+}
